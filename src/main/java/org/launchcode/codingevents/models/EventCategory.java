f9b6b434067c2284d52880e537eeaab4f5622658
Ben <bensztukowski@gmail.com>
2021-06-21 08:36:07 -0500

    ch18 thru 3 done

--- src/main/java/org/launchcode/codingevents/models/EventCategory.java
+++ src/main/java/org/launchcode/codingevents/models/EventCategory.java
@@ -1,7 +1,10 @@
 package org.launchcode.codingevents.models;
 
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotBlank;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class EventCategory extends AbstractEntity {
@@ -10,10 +13,17 @@ public class EventCategory extends AbstractEntity {
     @NotBlank(message = "Name is required")
     private String name;
 
+    @OneToMany(mappedBy = "eventCategory")
+    private final List<Event> events = new ArrayList<>();
+
     public EventCategory(){}
 
     public EventCategory(String name) {
         this.name = name;
+    }
+
+    public List<Event> getEvents() {
+        return events;
     }
 
     public String getName() {
